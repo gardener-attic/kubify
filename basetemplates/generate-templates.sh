5c9c0c70395879f1dfe8c4e475a1156bd00be11b
Andreas Fritzler <andreas.fritzler@sap.com>
2018-01-15 12:52:49 +0100

    Fixed basedir in generation script

--- basetemplates/generate-templates.sh
+++ basetemplates/generate-templates.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Render the latest bootkube templates and replace the old one in this directory.
 
+BASEDIR="$(dirname "$0")"
+
 BOOTKUBE_VERSION=v0.9.1
 
 docker run -it quay.io/coreos/bootkube:$BOOTKUBE_VERSION /bootkube render \
     --asset-dir /generated-templates/ \
     --experimental-self-hosted-etcd \
-    && docker cp $(docker ps --last 1 -q):/generated-templates/ ./
+    && docker cp $(docker ps --last 1 -q):/generated-templates/ $BASEDIR
 
 # Remove old directories
-rm -rf auth/ bootstrap-manifests/ etcd/ manifests/ tls/
+rm -rf $BASEDIR/auth/ $BASEDIR/bootstrap-manifests/ $BASEDIR/etcd/ $BASEDIR/manifests/ $BASEDIR/tls/
 
 # Move generated files into place
-cp -r generated-templates/* .
-rm -rf generated-templates/
+cp -r $BASEDIR/generated-templates/* .
+rm -rf $BASEDIR/generated-templates/
