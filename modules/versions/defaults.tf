c61ea7e0979586768d5238d8ea2964f86614a37d
Andreas Fritzler <andreas.fritzler@sap.com>
2018-03-06 09:10:56 +0100

    Bumped kubernetes version default to 1.9.3

--- modules/versions/defaults.tf
+++ modules/versions/defaults.tf
@@ -19,7 +19,7 @@
 locals {
   image_name                 = "coreos-1520.6.0"
   bastion_image_name         = "ubuntu-16.04"
-  kubernetes_version         = "v1.8.6"
+  kubernetes_version         = "v1.9.3"
   dns_version                = "1.14.5"
   flannel_version            = "v0.9.1"
   cni_version                = "0.3.0"
